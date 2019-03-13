24396046d71ce36210f317ff4986b60603cff873
sbushman <steve@stevebushman.com>
2019-03-13 12:17:37 -0500

    basically working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
 import {Button} from './components/Button'
 import {Button2} from './components/Button2'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h2>
-          This is a test of React with Typescript / CSS Modules / SASS
-        </h2>
-      
-        <Button title='button 1' />
-        <Button title='another button' />
-        <Button2 title='differently styled button' />
-      </div>
-    );
-  }
+import { BrowserRouter as Router, Route, Link, Switch as RouteSwitch, BrowserRouter } from "react-router-dom";
+
+const Welcome = () => {
+  return (
+    <h2>Welcome from React!</h2>
+  )
+}
+const NotWelcome = () => {
+  return (
+    <h2>Not on welcome page!</h2>
+  )
+}
+
+const App = (props: {}) => {
+
+  return (
+    <div className="App">
+
+      <BrowserRouter>
+        <RouteSwitch>
+          <Route exact path="/welcome_screen" component={Welcome} />
+          <Route component={NotWelcome} />
+        </RouteSwitch>
+      </BrowserRouter>
+
+  
+
+      <h2>
+        This is a test of React with Typescript / CSS Modules / SASS
+      </h2>
+    
+      <Button title='button 1' />
+      <Button title='another button' />
+      <Button2 title='differently styled button' />
+    </div>
+  )
+
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,28 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+let isReactInjected = false
+
+const tryInject = () => {
+  if(isReactInjected) { return }
+  console.log('checking for react')
+  let rootElement = document.getElementById('react-root') || document.getElementById('root')
+  if(rootElement) {
+    console.log('injecting react')
+    console.log('root element is', rootElement)   
+    ReactDOM.render(<App />, rootElement);   
+    isReactInjected = true
+  }
+}
+
+
+tryInject()
+const intevalTimerId = setInterval(() => {
+    tryInject()  
+}, 250)
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
